5dbccc90cbe3c9100eac93d079cd0bcb636eed25
revolution9 <dgainsworth@shaw.ca>
2025-12-02 13:02:20 -0700

    Add RetroPad.zig with Windows XP Notepad-like menus support

---
+++ RetroPad.zig
@@ -0,0 +1,10 @@
+// RetroPad.zig
+// This is a lightweight application with Windows XP Notepad-like menu support.
+
+const std = @import("std");
+
+pub fn main() void {
+    // Top-level menu controls: File, Edit, Help, etc.
+    std.debug.print("Welcome to RetroPad!\n", .{});
+    // Here we would implement menu functionality...
+}
